e34dc27782550de88df7e0678a6a79f9b81f0796
LABODJA <samirlabodja@gmail.com>
2023-03-16 15:22:19 +0000

    controle verifié

--- script.js
+++ script.js
@@ -12,41 +12,112 @@ iconeClose.addEventListener('click', ()=> {
 }
 );
 
-document.getElementById("inscription").addEventListener("submit", function(e){
+const form = document.getElementById("inscription");
+const prenomInput = document.getElementById("prenom");
+const nomInput = document.getElementById("nom");
+const emailInput = document.getElementById("email");
+const dateNaissInput = document.getElementById("dateNaiss");
+const nbrPartInput = document.getElementById("nbrPart");
 
-    var erreur;
-    var prenom = document.getElementById("prenom");
-    var nom = document.getElementById("nom");
-    var email = document.getElementById("email");
-    var dateNaiss = document.getElementById("dateNaiss");
-    var nbrPart = document.getElementById("nbrPart");
+const prenomError = document.getElementById("prenom-error");
+const nomError = document.getElementById("nom-error");
+const emailError = document.getElementById("email-error");
+const dateNaissError = document.getElementById("dateNaiss-error");
+const nbrPartError = document.getElementById("nbrPart-error");
 
-    if(!nbrPart.value){
-        erreur="Veuillez renseigner votre nombre de participation";
-    }
 
-    if(!dateNaiss.value){
-        erreur="Veuillez renseigner votre date de naissance";
-    }
+form.addEventListener("submit", function(event) {
+  // Reset error messages
+  prenomError.textContent = "";
+  nomError.textContent = "";
+  emailError.textContent = "";
+  dateNaissError.textContent = "";
+  nbrPartError.textContent = "";
 
-    if(!email.value){
-        erreur="Veuillez renseigner votre email";
-    }
+  let formIsValid = true;
 
-    if(!nom.value){
-        erreur="Veuillez renseigner votre nom";
-    }
+  // Validate prenom
+  if (prenomInput.value.trim() === "") {
+    prenomError.textContent = "Le prénom est obligatoire";
+    prenomError.style.color = "red";
+    prenomInput.style.borderColor = "red";
+    event.preventDefault();
+    formIsValid = false;
+  } else if (prenomInput.value.trim().length < 3) {
+    prenomError.textContent = "Le prénom doit avoir plus de 3 caractères";
+    prenomError.style.color = "red";
+    prenomInput.style.borderColor = "red";
+    event.preventDefault();
+    formIsValid = false;
+  } else if (/\d/.test(prenomInput.value)) {
+    prenomError.textContent = "Le prénom ne doit pas contenir de chiffre";
+    prenomError.style.color = "red";
+    prenomInput.style.borderColor = "red";
+    event.preventDefault();
+    formIsValid = false;
 
-    if(!prenom.value){
-        erreur="Veuillez renseigner votre prenom";
-    }
+  }
 
-    if(erreur){
-        e.preventDefault();
-        document.getElementById("erreur").innerHTML = erreur;
-        return false;
-    }else{
-        alert('Validé!');
-    }
+  // Validate nom
+  if (nomInput.value.trim() === "") {
+    nomError.textContent = "Le nom est obligatoire";
+    nomError.style.color = "red";
+    nomInput.style.borderColor = "red";
+    event.preventDefault();
+    formIsValid = false;
+  } else if (nomInput.value.trim().length < 3) {
+    nomError.textContent = "Le nom doit avoir plus de 3 caractères";
+    nomError.style.color = "red";
+    nomInput.style.borderColor = "red";
+    event.preventDefault();
+    formIsValid = false;
+  } else if (/\d/.test(nomInput.value)) {
+    nomError.textContent = "Le nom ne doit pas contenir de chiffre";
+    nomError.style.color = "red";
+    nomInput.style.borderColor = "red";
+    event.preventDefault();
+    formIsValid = false;
+  }
 
-})
+  //Validate email
+  // Regex to validate email format
+const emailRegex = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
+if (emailInput.value.trim() === "" || !emailRegex.test(emailInput.value.trim())) {
+emailError.textContent = "Veuillez saisir une adresse email valide";
+emailError.style.color = "red";
+emailInput.style.borderColor = "red";
+event.preventDefault();
+formIsValid = false;
+}
+
+// Validate dateNaiss
+if (dateNaissInput.value.trim() === "") {
+    dateNaissError.textContent = "La date de naissance est obligatoire";
+    dateNaissError.style.color = "red";
+    dateNaissInput.style.borderColor = "red";
+    event.preventDefault();
+    formIsValid = false;
+  }
+  
+  // Validate nbrPart
+  if (nbrPartInput.value.trim() === "" || nbrPartInput.value < 0) {
+    nbrPartError.textContent = "Le nombre de tournoi est obligatoire et doit être supérieur ou égal à 0";
+    nbrPartError.style.color = "red";
+    nbrPartInput.style.borderColor = "red";
+    event.preventDefault();
+    formIsValid = false;
+  }
+
+  
+  // Valider la sélection de la ville
+  let selectedCity = form.querySelector('input[name="city"]:checked');
+  if (!selectedCity) {
+    form.querySelector('.name-field__radiobtn').style.borderColor = "red";
+    formIsValid = false;
+  }
+
+ 
+
+
+});
+
